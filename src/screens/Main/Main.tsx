dd5c51a0c5457cc981140589d88bca6c7542292e
Alex Meshok <alexandermeshok@coherentsolutions.com>
2025-10-14 16:00:36 +0300

    added expo review

--- src/screens/Main/Main.tsx
+++ src/screens/Main/Main.tsx
@@ -1,8 +1,9 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from 'react';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import React from 'react';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
 import { ADD_LIST_ROUTE, ADD_TASK_ROUTE, FULL_TASK_ROUTE, HOME_ROUTE } from '~constants/routes';
 import Home from '~screens/Home';
 import AddList from '~screens/Home/AddList';
@@ -10,6 +11,8 @@ import AddTask from '~screens/Home/AddTask';
 import FullTask from '~screens/Home/FullTask';
 import Modals from '~screens/Modals/Modals';
 import BannerAd from '~UI/BannerAd';
+
+import ReviewPrompt from './ReviewPrompt';
 import UpdateAppAlert from './UpdateAppAlert';
 
 const Main = () => {
@@ -27,6 +30,7 @@ const Main = () => {
           </Stack.Navigator>
           <Modals />
           <UpdateAppAlert />
+          <ReviewPrompt />
           <BannerAd />
         </NavigationContainer>
       </SafeAreaView>
